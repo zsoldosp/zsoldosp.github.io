84565428193fefe7031cb55c2d8f2919b23434ab
zsoldosp <peter.zsoldos@gmail.com>
2014-02-01 20:56:37 +0100

    automating an annoying manual step of removing the site verification tokens

--- _update_from_site.sh
+++ _update_from_site.sh
@@ -3,3 +3,4 @@ ls | grep -v "\(README.md\|_site\|`basename $0`\)" | while read f; do rm -rf $f;
 mv _site/* .
 find . -name index.xml | while read f; do git diff $f | grep -v "<pubDate>" | grep ^"[+-] " > /dev/null || git checkout -- $f ; done
 rm -rf _site
+git checkout -- BingSiteAuth.xml google6c8b0f55173f9979.html
