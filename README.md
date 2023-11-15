1d8a29a68800b9f47858534a535ee1541c098297
Peter Zsoldos <hello@zsoldosp-eu>
2023-11-15 18:17:53 +0000

    posthog tracking

--- README.md
+++
@@ -1,4 +0,0 @@
-zsoldosp.eu
-===========
-
-Peter Zsoldos's blog (hosted by github-pages at http://zsoldosp.eu
--- _update_from_site.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-ls | grep -v "\(README.md\|_site\|`basename $0`\)" | while read f; do rm -rf $f; done
-mv _site/* .
-find . -name index.xml | while read f; do git diff $f | grep -v "<pubDate>" | grep ^"[+-] " > /dev/null || git checkout -- $f ; done
-rm -rf _site
-git checkout -- BingSiteAuth.xml google6c8b0f55173f9979.html
